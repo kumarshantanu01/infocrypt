660f74224bacf03c89390d8761f8ba43ba26eff7
kumarshantanu01 <shantanuk280@gmail.com>
2022-01-27 02:43:11 +0530

    feat: adding navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,26 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <h1>footer</h1>
+      <footer class="page-footer font-small panel-primary">
+        <div class="container">
+          <ul class="list-unstyled list-inline text-center py-2">
+            <li class="list-inline-item">
+              <h5 class="mb-1">Register for free</h5>
+            </li>
+            <li class="list-inline-item">
+              <a href="#!" class="btn btn-outline-white btn-rounded">
+                Sign up!
+              </a>
+            </li>
+          </ul>
+        </div>
+        
+
+        <div class="footer-copyright text-center py-3">
+          © 2020 Copyright:
+          <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,42 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div>
-      <ul>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/news">
-          <li>News</li>
-        </Link>
-      </ul>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <Link to="/">
+                  <a class="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/news">
+                  <a class="nav-link" href="#">
+                    News
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
