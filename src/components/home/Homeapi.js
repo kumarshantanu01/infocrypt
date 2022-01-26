2d24d033b2fc885b181334e5531460bd4bdb925a
kumarshantanu01 <shantanuk280@gmail.com>
2022-01-27 03:39:22 +0530

    fix: deplotyment

--- src/components/home/Homeapi.js
+++ src/components/home/Homeapi.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState } from "react";
 
-function Homeapi() {
-  return <div></div>;
+function NewsApi() {
+  const [news, setNews] = useState([]);
+
+  async function asyncFunc() {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/everything?qInTitle=crypto&sortBy=popularity&apiKey=ed82c3c548024e7bb58f1a4e07ac26b1"
+      );
+
+      console.log(response);
+      setNews(response.data.articles);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  asyncFunc();
+
+  return (
+    <>
+      <div className="container mt-4">
+        <div className=" text-center">
+          <h3>News articles related to crypto </h3>
+        </div>
+        <div className="container mt-4">
+          {news.map((value) => {
+            return (
+              <div className="card mb-3" style={{ width: "auto" }}>
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img
+                      src={value.urlToImage}
+                      className="img-fluid rounded-start"
+                      alt="..."
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">{value.title}</h5>
+                      <p className="card-text">
+                        <small className="text-muted">
+                          {value.description}
+                        </small>
+                      </p>
+                      <p className="card-text">{value.content}</p>
+                      <button className="btn btn-info">
+                        <a href={value.url} target="blank">
+                          Read More
+                        </a>
+                      </button>
+                      <p className="card-text">
+                        <small className="text-muted ">
+                          {value.author}
+                          <p>Published on :{value.publishedAt}</p>
+                        </small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Homeapi;
+export default NewsApi;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import NewsApi from "../components/home/Homeapi";
 
 function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <NewsApi/>
     </div>
   );
 }
