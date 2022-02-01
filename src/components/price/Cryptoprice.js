4afa6f6ea89e1d648cd992b25092fabf9b716372
kumarshantanu01 <shantanuk280@gmail.com>
2022-02-01 19:34:47 +0530

    displaying data from API

--- src/components/price/Cryptoprice.js
+++ src/components/price/Cryptoprice.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Coincard from './Coincard';
 
+// import axios from "axios";
 function Cryptoprice() {
+  const [coins, setCoins] = useState();
+  const [dataisloaded, setDataisloaded] = useState(false);
+
+  async function fetch_coins() {
+    
+    await fetch(
+      "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=12&offset=0",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+          "x-rapidapi-key":
+            "ea7fce0598mshef181fd5a014e36p1bbc0djsn1a9bac13ef3f",
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((res) => {
+        setCoins(res.data.coins);
+        setDataisloaded(true);
+        console.log(res.data.coins);
+      });
+  }
+
+  useEffect(() => {
+    fetch_coins();
+  }, []);
+
   return (
     <div>
       <h1>live prices will be displayed here</h1>
+      <div>
+        <Coincard coins={coins} dataisloaded={dataisloaded} />
+      </div>
     </div>
   );
 }
